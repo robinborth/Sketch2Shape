3d92c9019add32e57f6cf3487adb42b85ad271c1
Daniel Korth <daniel.korth@tum.de>
2023-11-28 15:16:38 +0100

    change reconstruction pipeline

--- scripts/reconstruct_objects_deepsdf.py
+++ scripts/reconstruct_objects_deepsdf.py
@@ -32,15 +32,19 @@ def load_shape2idx(path: str):
     return shape2idx
 
 
-def identify_matching_objs(gt_path, rec_paths):
-    matches = list()
-    for rec in rec_paths:
-        # find gt
-        folder = rec.split("/")[-1].replace("_", "/").split("/")[:-1]
-        full_gt_path = os.path.join(gt_path, *folder, "model_unit_sphere.obj")
+# def identify_matching_objs(gt_path, rec_paths):
+#     matches = list()
+#     for rec in rec_paths:
+#         # find gt params
+#         folder = rec.split("/")[-1].replace("_", "/").split("/")[:-1]
+#         full_gt_path = os.path.join(gt_path, *folder, "model_unit_sphere.obj")
 
-        matches.append((full_gt_path, rec))
-    return matches
+#         matches.append((full_gt_path, rec))
+#     return matches
+
+
+def get_id_from_path(rec_path):
+    return rec_path.split("/")[-1].split("_")[0]
 
 
 @hydra.main(version_base=None, config_path="../conf", config_name="reconstruct_deepsdf")
@@ -65,21 +69,28 @@ def reconstruct(cfg: DictConfig) -> None:
     )
 
     if cfg.chamfer:
-        matches = identify_matching_objs(run_config.data.data_dir, rec_paths)
         chamfers = dict()
-        for match in matches:
+        for rec_path in rec_paths:
+            obj_id = get_id_from_path(rec_path)
+            norm_file = np.load(run_config.data.norm_dir + "/" + obj_id + ".npz")
+
             # load meshes
-            gt = trimesh.load(match[0], force="mesh")
-            rec = trimesh.load(match[1], force="mesh")
+            gt = trimesh.load(run_config.data.surf_dir + "/" + obj_id + ".ply")
+            rec = trimesh.load(rec_path)
+
+            # TODO should be done somewhere else, but fine for now
+            rec.vertices = rec.vertices / norm_file["scale"] - norm_file["offset"]
+            rec.export(rec_path + ".obj")
+
             chamfer = compute_chamfer_distance(
                 gt, rec, n_samples=cfg.chamfer_num_samples
             )
-            idx = match[0].split("/")[-2]
-            chamfers[idx] = chamfer
+
+            chamfers[obj_id] = chamfer
         chamfers["mean_chamfer"] = np.mean(list(chamfers.values()))
         chamfers["num_samples"] = cfg.chamfer_num_samples
 
-        np.savez(cfg.log_path + "/chamfers.npz", chamfers)
+        np.savez(cfg.log_path + "/chamfers.npz", chamfers=chamfers)
         print(f"Mean Chamfer distance over all objects is: {chamfers['mean_chamfer']}")
 
 
