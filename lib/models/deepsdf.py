c9f9d6fcaa0a8599dc577c545bf6fd7672d4e599
Robin Borth <borth.private@tum.de>
2024-01-03 16:14:48 +0100

    feat: train deepsdf

--- lib/models/deepsdf.py
+++ lib/models/deepsdf.py
@@ -39,7 +39,7 @@ class DeepSDF(L.LightningModule):
         super().__init__()
         # this line allows to access init params with 'self.hparams' attribute
         # also ensures init params will be stored in ckpt
-        self.save_hyperparameters()
+        self.save_hyperparameters(logger=False)
         self.automatic_optimization = False
 
         self.loss = loss
