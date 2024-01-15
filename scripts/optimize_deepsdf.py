20400730d3761dee1bb18ef6cc39d1fd42669dd8
Robin Borth <borth.private@tum.de>
2024-01-15 18:27:59 +0100

    feat: multirun

--- scripts/optimize_deepsdf.py
+++ scripts/optimize_deepsdf.py
@@ -25,53 +25,61 @@ def optimize(cfg: DictConfig) -> None:
     L.seed_everything(cfg.seed)
 
     log.info(f"==> initializing datamodule <{cfg.data._target_}>")
-    metainfo = MetaInfo(data_dir=cfg.data.data_dir)
-    obj_id = metainfo.obj_ids[0]
-    datamodule: LightningDataModule = hydra.utils.instantiate(cfg.data, obj_id=obj_id)
+    metainfo = MetaInfo(data_dir=cfg.data.data_dir, split="train")
+    for obj_id in metainfo.obj_ids:
+        log.info(f"==> optimize {obj_id=} ...")
+        datamodule: LightningDataModule = hydra.utils.instantiate(
+            cfg.data,
+            obj_id=obj_id,
+        )
 
-    log.info(f"==> initializing model <{cfg.model._target_}>")
-    model: LightningModule = hydra.utils.instantiate(cfg.model)
+        log.info(f"==> initializing model <{cfg.model._target_}>")
+        model: LightningModule = hydra.utils.instantiate(cfg.model)
 
-    log.info("==> initializing callbacks ...")
-    callbacks: List[Callback] = instantiate_callbacks(cfg.get("callbacks"))
+        log.info("==> initializing callbacks ...")
+        callbacks: List[Callback] = instantiate_callbacks(cfg.get("callbacks"))
 
-    log.info("==> initializing logger ...")
-    logger: WandbLogger = instantiate_loggers(cfg.get("logger"))
-    if logger is not None:
-        logger.watch(model, log="all")
+        log.info("==> initializing logger ...")
+        logger: WandbLogger = instantiate_loggers(cfg.get("logger"))
+        if logger is not None:
+            logger.watch(model, log="all")
 
-    log.info(f"==> initializing trainer <{cfg.trainer._target_}>")
-    trainer: Trainer = hydra.utils.instantiate(
-        cfg.trainer,
-        callbacks=callbacks,
-        logger=logger,
-    )
+        log.info(f"==> initializing trainer <{cfg.trainer._target_}>")
+        trainer: Trainer = hydra.utils.instantiate(
+            cfg.trainer,
+            callbacks=callbacks,
+            logger=logger,
+        )
 
-    object_dict = {
-        "cfg": cfg,
-        "datamodule": datamodule,
-        "model": model,
-        "callbacks": callbacks,
-        "logger": logger,
-        "trainer": trainer,
-    }
-    if logger:
-        log.info("==> logging hyperparameters ...")
-        log_hyperparameters(object_dict)
+        object_dict = {
+            "cfg": cfg,
+            "datamodule": datamodule,
+            "model": model,
+            "callbacks": callbacks,
+            "logger": logger,
+            "trainer": trainer,
+        }
+        if logger:
+            log.info("==> logging hyperparameters ...")
+            log_hyperparameters(object_dict)
 
-    if cfg.train:
-        log.info("==> start training ...")
-        trainer.fit(model=model, datamodule=datamodule)
+        if cfg.train:
+            log.info("==> start training ...")
+            trainer.fit(model=model, datamodule=datamodule)
 
-    if cfg.test:
-        log.info("==> start evalution ...")
-        trainer.test(model=model, datamodule=datamodule)
+        if cfg.test:
+            log.info("==> start evalution ...")
+            trainer.test(model=model, datamodule=datamodule)
 
-    if cfg.save_mesh and (mesh := model.mesh):
-        log.info("==> save mesh ...")
-        path = Path(cfg.paths.mesh_dir, f"{obj_id}.obj")
-        path.parent.mkdir(parents=True, exist_ok=True)
-        o3d.io.write_triangle_mesh(path.as_posix(), mesh=mesh, write_triangle_uvs=False)
+        if cfg.save_mesh and (mesh := model.mesh):
+            log.info("==> save mesh ...")
+            path = Path(cfg.paths.mesh_dir, f"{obj_id}.obj")
+            path.parent.mkdir(parents=True, exist_ok=True)
+            o3d.io.write_triangle_mesh(
+                path.as_posix(),
+                mesh=mesh,
+                write_triangle_uvs=False,
+            )
 
 
 if __name__ == "__main__":
