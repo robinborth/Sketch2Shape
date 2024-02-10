0cdaad7b84b77568c72284c062c328d8a8070d5d
Robin Borth <borth.private@tum.de>
2024-02-10 18:33:10 +0100

    refactor the optimize dataset into seperate files

--- lib/data/optimize_latent_datamodule.py
+++ lib/data/optimize_latent_datamodule.py
@@ -4,34 +4,31 @@ from lightning import LightningDataModule
 from torch.utils.data import DataLoader, Dataset
 
 
-class DeepSDFDataModule(LightningDataModule):
+class LatentOptimizationDataModule(LightningDataModule):
     def __init__(
         self,
-        # settings
-        data_dir: str = "data/",
-        chunk_size: int = 16384,
-        half: bool = False,
+        obj_id: str = "obj_id",
         # training
-        batch_size: int = 32,
+        batch_size: int = 1,
         num_workers: int = 0,
         pin_memory: bool = False,
         drop_last: bool = True,
         persistent_workers: bool = False,
         shuffle: bool = False,
         # dataset
-        dataset: Optional[Dataset] = None,
+        train_dataset: Optional[Dataset] = None,
+        eval_dataset: Optional[Dataset] = None,
         **kwargs,
     ) -> None:
         super().__init__()
         self.save_hyperparameters(logger=False)
+        self.obj_id = obj_id
 
     def setup(self, stage: str) -> None:
-        self.train_dataset = self.hparams["dataset"](
-            data_dir=self.hparams["data_dir"],
-            split="train",
-            chunk_size=self.hparams["chunk_size"],
-            half=self.hparams["half"],
-        )
+        if stage == "fit":
+            self.train_dataset = self.hparams["train_dataset"](obj_id=self.obj_id)
+        if stage == "test":
+            self.eval_dataset = self.hparams["eval_dataset"](obj_id=self.obj_id)
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(
@@ -42,4 +39,13 @@ class DeepSDFDataModule(LightningDataModule):
             drop_last=self.hparams["drop_last"],
             persistent_workers=self.hparams["persistent_workers"],
             shuffle=self.hparams["shuffle"],
+        )
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(
+            dataset=self.eval_dataset,
+            batch_size=1,
+            num_workers=self.hparams["num_workers"],
+            pin_memory=self.hparams["pin_memory"],
+            persistent_workers=self.hparams["persistent_workers"],
         )
